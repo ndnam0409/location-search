a026ea60fcda7ea2a36c27c5159aa0b1f2cd4337
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2024-04-10 16:19:32 +0700

    finished

---
+++ src/components/LocationSearch.tsx
@@ -0,0 +1,53 @@
+import { Fragment, useState } from "react";
+import type { Place } from "../api/Place";
+import { search } from "../api/search";
+
+interface LocationSearchProps {
+  onPageClick: (place: Place) => void;
+}
+
+const LocationSearch = ({ onPageClick }: LocationSearchProps) => {
+  const [term, setTerm] = useState("");
+  const [places, setPlaces] = useState<Place[]>([]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const res = await search(term);
+    setPlaces(res);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+        <input
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+      <h1 className="font-bold mt-6">Found Locations</h1>
+      <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+        {places.map((place) => {
+          return (
+            <Fragment key={place.id}>
+              <p className="text-sm">{place.name}</p>
+              <button
+                className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded-full"
+                onClick={() => onPageClick(place)}
+              >
+                Go
+              </button>
+              <div className="border-b w-full col-span-2" />
+            </Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default LocationSearch;
---
+++ src/components/Map.tsx
@@ -0,0 +1,34 @@
+import { Map as LeafletMap } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useRef } from "react";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import type { Place } from "../api/Place";
+
+interface MapProps {
+  place: Place | null;
+}
+
+const Map = ({ place }: MapProps) => {
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[40.7, -74]}
+      zoom={12}
+      scrollWheelZoom
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && <Marker position={[place.latitude, place.longitude]} />}
+    </MapContainer>
+  );
+};
+
+export default Map;
